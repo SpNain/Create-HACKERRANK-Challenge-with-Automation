7080cfc4a35f73ac791ae6cd8abeb25c8ae9ed09
HardeepNain <hnspnain@gmail.com>
2023-02-25 23:58:31 +0530

    v1 org explained

---
+++ README.md
@@ -0,0 +1,17 @@
+# Create HACKERRANK Challenge with Automation
+
+The project automates the process of creating `HackerRank` challenges using `Puppeteer` and Node.js.
+
+It utilizes the concept of `promises` to make the code asynchronous.
+
+## Working 📝
+
+- We have a object file which contains different problems as objects.
+- With the help of Node.js we automate the process of creating challenge.
+- It select problems from object file one by one and add those as challenges in hackerrank.
+- It also add moderator name in the challenges.
+- The project has the potential to save time and resources for organizations that frequently use HackerRank for hiring and skill assessments.
+
+## Tech Stack ⚒
+
+**Javascript, Nodejs & Puppeteer**
--- moderatorAdd.js
+++ moderatorAdd.js
@@ -25,11 +25,18 @@ async function login(){
     let bothLis = await tab.$$('.nav-tabs.nav.admin-tabbed-nav li');
     let manageChallengeLi = bothLis[1];
     await manageChallengeLi.click();
+    // ye uper wala code challenges wali file se uthaya hai || isse hum manage challenges wale page pe chle jaate hai
     await addModerators(browser , tab);
 };
 login();
 
-
+/*
+addModerators() : iske pass browser aur tab aayenge 
+                us tab pe present saare questions ke links nikal dega
+                aur ek ek krke saare questions pe moderators add kr dega
+                aur agr agge koi page hai to uspe jaake moderator ko call lg jaati hai 
+                jisse ye code recursive bn jaata hai aur ek ek krke saare tabs ke questions pe moderators add hote rehte hai
+*/
 async function addModerators(browser , tab){
     await tab.waitForSelector('.backbone.block-center' , {visible:true});
     let allATags = await tab.$$('.backbone.block-center');
@@ -43,34 +50,34 @@ async function addModerators(browser , tab){
     for(let i=0 ; i<allQuesLinks.length ; i++){
         let qLink = allQuesLinks[i];
         let newTab = await browser.newPage();
-        await addModeratorToASingleQues(newTab , qLink);
-    }
+        await addModeratorToASingleQues(newTab , qLink); //Kyonki iske aage humne await likh diya hai to pahle ye function chalega fir jaake i increase hoga
+    }                                                    // jisse ek time pe ek hi ques pe kaam hoga
 
     // next button active hai to click on next
     // addModerators(browser , tab);
 
-    let allLis = await tab.$$('.pagination li');
-    let nextBtnLi = allLis[allLis.length-2];
-    let isDisabled = await tab.evaluate( function(elem){ return elem.classList.contains("disabled");  } , nextBtnLi );
+    let allLis = await tab.$$('.pagination li'); // saari lis nikal li pages ki 
+    let nextBtnLi = allLis[allLis.length-2];   // ye last se 2nd button hai jispe click krne se next page aata hai 
+    let isDisabled = await tab.evaluate( function(elem){ return elem.classList.contains("disabled");  } , nextBtnLi );  // hum check krte hai ki kya iss button ki classlist me disabled class hai?? 
     // if true ??
-    if(isDisabled){
+    if(isDisabled){ // agr disabled class hai to that means aage koi page nhi hai return kr jaao 
         return;
     }
-    // else false ??
-    await nextBtnLi.click();
+    // else false ?? // agr nhi hai to means aage page hai 
+    await nextBtnLi.click();  // hum button pe click krte hai jo hume next page pe le jaata hai
     await tab.waitForTimeout(5000);
-    await addModerators(browser , tab);
+    await addModerators(browser , tab);  // aur fir us page pe jaake hum dobara se moderator ko call lga dete hai
 }
 
-
+// ek single ques pe moderator add krta hai
 async function addModeratorToASingleQues(newTab , qLink){
        await newTab.goto(qLink);
        await newTab.waitForTimeout(2000);
-       await newTab.click('li[data-tab="moderators"]');
-       await newTab.waitForSelector('#moderator' , {visible:true});
+       await newTab.click('li[data-tab="moderators"]');  // moderator page
+       await newTab.waitForSelector('#moderator' , {visible:true});  // input box
        await newTab.type("#moderator" , "pep");
-       await newTab.click('.btn.moderator-save');
-       await newTab.click('.save-challenge.btn.btn-green');
+       await newTab.click('.btn.moderator-save');  // add button
+       await newTab.click('.save-challenge.btn.btn-green');  // save button
        await newTab.waitForTimeout(2000);
        await newTab.close();
 }
